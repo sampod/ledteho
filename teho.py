98ea8d8d018ebb21f9a3998af5251796df6120e2
sampod <sami.heino@gmail.com>
2019-02-28 21:11:31 +0000

    Changed to python3, separated write routine

--- teho.py
+++ teho.py
@@ -1,16 +1,20 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import time
 import os
 GPIO.setmode(GPIO.BCM)
 
 # Global variables
-global lastSignal, lastTime, power, difference, powercap
+global lastSignal, lastTime, power, difference, powercap, counter
 lastSignal = False
 lastTime = time.time()
 power = 0
 powercap = 100
 difference = 0
+
+# files
+tehofile = "/ramdisk/teho.txt"
+counterfile = "/ramdisk/tehocounter.txt"
 
 # GPIO 26 set up as input, pulled down to avoid false detection.
 # GPIO 26 connected to automationPhat input 1
@@ -21,38 +25,44 @@ GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # routine to calculate power and increase energy counter when pulse
 # is detected
 def pulsecallback(channel):
-    global lastSignal, lastTime, power, difference
-    oldcounter = 0
-    counter = 0
+    global lastSignal, lastTime, power, difference, counter
     seconds_in_an_hour = 3600
     meter_constant = 1000
-    print "falling edge detected on 26"
+    print ("falling edge detected on 26")
     newTime = time.time()
     difference = newTime - lastTime
     power = seconds_in_an_hour / (difference * meter_constant)
     if power > powercap: power = ""
-    teho = round(power, 3)
     lastTime = newTime
-    print power, difference
+    print (power, difference)
+    counter = int(counter) + 1
+    print (counter)
 
-    tehofile = open("/ramdisk/teho.txt", "w")
-    tehofile.write(str(teho))
-    tehofile.close
+# routine to write stuff to file 
+# called in main loop on regular basis!
+def writetofile():
+    global power, counter
+    global tehofile, counterfile
 
-    exists = os.path.isfile('/ramdisk/tehocounter.txt')
+    teho = round(power, 3)
 
-    if exists:
-        counterfile = open("/ramdisk/tehocounter.txt", "r")
-        oldcounter = counterfile.read()
-        counterfile = open("/ramdisk/tehocounter.txt", "w")
-    else:
-        counterfile = open("/ramdisk/tehocounter.txt", "w")
+    print ("writing...")
 
-    print oldcounter
-    counter = int(oldcounter) + 1
-    counterfile.write(str(int(oldcounter) + 1))
-    counterfile.close
+    with open(tehofile, "w") as f:
+        f.write(str(teho))
 
+    with open(counterfile, "w") as f:
+        f.write(str(counter))
+
+# laskurin alustus
+
+exists = os.path.isfile(counterfile)
+
+if exists:
+    with open(counterfile, "r") as f:
+        counter = f.read()
+else:
+    counter = 0
 
 # when a falling edge is detected on port 26,
 # the function pulsecallback will be run
@@ -64,13 +74,15 @@ GPIO.add_event_detect(26, GPIO.FALLING, callback=pulsecallback, bouncetime=300)
 
 # main program loop.
 # does nothing but sleeps all day
-print "waiting for pulses in GPIO 26"
-print "Use ctrl+z to interrupt"
+print ("waiting for pulses in GPIO 26")
+print ("Use ctrl+z to interrupt")
 try:
     while True:
-        time.sleep(1e6)
+        time.sleep(10)
+        writetofile()
 
 except KeyboardInterrupt:
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+    writetofile()
 GPIO.cleanup()           # clean up GPIO on normal exit
-
+writetofile()
